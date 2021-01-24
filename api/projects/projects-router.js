0b0192a2e08e74112833d01daf1fc7ab4926bdf9
Mark Hillin <mark.hillin@gmail.com>
2021-01-24 12:03:31 -0600

    Projects Done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -17,5 +17,60 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", validateProjectId, async (req, res) => {
     const { id } = req.params;
-    
+    try {
+        const actions = await projectsModel.get(id);
+        res.status(201).json(actions)
+    } catch {
+        res.status(500).json({ message: "Problem with the server" })
+    }
+});
+
+router.get("/:id/actions", validateProjectId, async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const actions = await projectsModel.getProjectActions(id);
+        res.status(201).json(actions)
+    } catch {
+        res.status(500).json({ message: "Problem with the server" })
+    }
 })
+
+router.post ("/", async (req, res) => {
+    const project = req.body;
+    if (project) {
+        try {
+            const newProject = await projectsModel.insert(project);
+            res.status(201).json(newProject)
+        } catch {
+            res.status(500).json({ message: "Problem with the server" })
+        }
+    } else {
+        res.status(400).json({ message: "Problem with the request" });
+    }
+})
+
+router.put("/:id", validateProjectId, async (req, res) => {
+    const project = req.body;
+    const { id } = req.params;
+
+    try {
+        const update = await projectsModel.update(id, project);
+        res.status(201).json(update)
+    } catch {
+        res.status(500).json({ message: "Problem with the server" })
+    }
+});
+
+router.delete ("/:id", validateProjectId, async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const actions = await projectsModel.remove(id);
+        res.status(201).json(actions)
+    } catch {
+        res.status(500).json({ message: "Problem with the server" })
+    }
+})
+
+module.exports = router;
