be4a4330fd0c98f5f678922b5dac8430b921654c
Mark Hillin <mark.hillin@gmail.com>
2021-01-24 11:47:41 -0600

    Actions Done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,21 @@
-// Write your "projects" router here!
+const express = require ("express");
+
+const router = express.Router();
+
+const projectsModel = require("./projects-model");
+
+const { validateProjectId } = require("../middleware/middleware");
+
+router.get("/", async (req, res) => {
+    try {
+        const projects = await projectsModel.get();
+        res.status(201).json(projects)
+    } catch {
+        res.status(500).json({ message: "Problem with the server" })
+    }
+});
+
+router.get("/:id", validateProjectId, async (req, res) => {
+    const { id } = req.params;
+    
+})
--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const server = express();
+const helmet = require("helmet");
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.use(helmet());
+server.use(express.json());
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
 
 module.exports = server;
